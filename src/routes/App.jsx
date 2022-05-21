2a7d9966ecf29cd1d08f0c5af9ffca83267da5aa
Weslen Mendes <weslenmends@gmail.com>
2022-05-21 01:01:50 -0300

    fix: addition of app.jsx

---
+++ src/routes/App.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import "reset-css"
+
+import StartNow from "./components/StartNow"
+import Chat from "./components/Chat"
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<StartNow />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
